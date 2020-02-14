84b34288b95568f704dc19525701138dee7ae622
Dustin Fedie <dlfedie@gmail.com>
2020-02-14 14:34:18 -0600

    adds remaining packages

--- Startup.cs
+++ Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using RazorPagesMovie.Models;
+using RazorPagesMovie.Data;
 using Microsoft.EntityFrameworkCore;
 
 namespace RazorPagesMovie
