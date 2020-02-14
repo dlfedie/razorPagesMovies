6d3029aecd1f4dc031104b0032c9807ab89f5022
Dustin Fedie <dlfedie@gmail.com>
2020-02-14 14:49:57 -0600

    adds sql/sqlite switch

--- Startup.cs
+++ Startup.cs
@@ -15,19 +15,30 @@ namespace RazorPagesMovie
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
-            services.AddDbContext<RazorPagesMovieContext>(options =>
-                options.UseSqlite(Configuration.GetConnectionString("MovieContext")));
+            if (Environment.IsDevelopment())
+            {
+                services.AddRazorPages();
+                services.AddDbContext<RazorPagesMovieContext>(options =>
+                    options.UseSqlite(Configuration.GetConnectionString("MovieContext")));
+            }
+            else
+            {
+                services.AddRazorPages();
+                services.AddDbContext<RazorPagesMovieContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MovieContext")));
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
