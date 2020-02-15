2e51211f03f7950acb63d64f23606b1d196733df
Dustin Fedie <dlfedie@gmail.com>
2020-02-15 14:35:42 -0600

    jfx ok, finally got the damn movies folder to create! was a connectionstring null error. i think switching between 2.2 and 3.1 versions of dotnet caused issue. or MS documentation is suspect

--- Data/RazorPagesMovieContext.cs
+++ Data/RazorPagesMovieContext.cs
@@ -4,11 +4,10 @@ namespace RazorPagesMovie.Data
 {
     public class RazorPagesMovieContext : DbContext
     {
-        public RazorPagesMovieContext(
+        public RazorPagesMovieContext (
             DbContextOptions<RazorPagesMovieContext> options)
             : base(options)
         {
-
         }
         public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; }
     }
